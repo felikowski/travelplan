0128fe036889e1965e4f0ce23f2765ee923395fa
felikowski <f.gloeckner@gmx.net>
2022-07-03 22:33:15 +0200

    feat(projectSetup): added typeorm to project
    I used the initializer of typeorm to build an typeorm ESM project including an express setup

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,42 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-const app = express();
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express"
+import bodyParser from "body-parser"
+import {Request, Response} from "express"
+import {AppDataSource} from "./db/data-source.js"
+import {Routes} from "./routes.js"
+import {User} from "./db/entity/User.js"
+import {fileURLToPath} from "url";
+import {dirname} from "path";
 import path from 'path';
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+AppDataSource.initialize().then(async () => {
 
-const port = 3000;
-app.use(express.static(path.join(__dirname, './ui/dist/ui')));
-app.use('*', (req, res) => {
-    const indexPath = path.join(__dirname, './ui/dist/ui/index.html');
-    res.sendFile(indexPath);
-});
-app.listen(port, () => console.log(`API server is listening on ${port}`));
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
+
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next)
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result)
+            }
+        })
+    })
+    app.use(express.static(path.join(__dirname, './ui/dist/ui')));
+    app.use('*', (req, res) => {
+        const indexPath = path.join(__dirname, './ui/dist/ui/index.html');
+        res.sendFile(indexPath);
+    });
+
+    // start express server
+    app.listen(3000)
+
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+
+}).catch(error => console.log(error))
